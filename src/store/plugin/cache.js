47b68868894e0acd7fdc25f6a4eca09987ed5532
chengwei ou <chengweiou@chengweis-MacBook-Pro.local>
2021-07-09 12:25:15 -0700

    change name in cache

--- src/store/plugin/cache.js
+++ src/store/plugin/cache.js
@@ -1,19 +1,19 @@
 export const order = 1
 export default store => {
-  Object.keys(store.state).filter(k => store.state[k].cache !== false).forEach(k => {
-    let cache = JSON.parse(sessionStorage.getItem('vuex.' + k))
+  Object.keys(store.state).filter(module => store.state[module].cache !== false).forEach(module => {
+    let cache = JSON.parse(sessionStorage.getItem(`vuex.${module}`))
     if (!cache) return
-    setProp(cache, store.state[k])
+    setProp(cache, store.state[module])
   })
 
   store.subscribe((mutation, state) => {
     let module = mutation.type.substring(0, mutation.type.indexOf('/'))
     if (state[module].cache === false) return
     if (mutation.payload === 'REMOVE') {
-      sessionStorage.removeItem('vuex.' + module)
+      sessionStorage.removeItem(`vuex${module}`)
       return
     }
-    sessionStorage.setItem('vuex.' + module, JSON.stringify(state[module]))
+    sessionStorage.setItem(`vuex.${module}`, JSON.stringify(state[module]))
   })
 }
 
