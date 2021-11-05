60eb150ea73fc06686f4135c0bd8be7b30c118d7
chengwei ou <chengweiou@chengweis-MacBook-Pro.local>
2021-11-04 23:34:11 -0700

    全局i18n还有效,要刷新页面才会切换,切换复合router

--- src/router/index.js
+++ src/router/index.js
@@ -9,14 +9,16 @@ export const router = createRouter({
       children: [
         { name: 'log', path: 'log', component: () => import('@/view/log/index.vue') },
         { name: 'refresh', path: 'refresh', component: () => import('@/view/refresh.vue') },
-        { name: 'login', path: '', component: () => import('@/view/login/index.vue') },
+        { name: 'home', path: '', component: () => import('@/view/xxx/list.vue') },
+        { name: 'xxxList', path: 'xxx', component: () => import('@/view/xxx/list.vue') },
+        { name: 'xxx', path: 'xxx/:id', component: () => import('@/view/xxx/detail.vue') },
+        { name: 'log', path: 'log', component: () => import('@/view/log/index.vue') },
       ],
     },
     {
       path: '/test',
       components: { full: () => import('./admin.vue') },
       children: [
-        { name: 'restapi', path: 'restapi', component: () => import('@/view/restapi/index.vue') },
       ],
     },
   ],
